1e4dc9a18a379e73e6c9b16a59fffe6070d94908
mr-axelma <83195212+mr-axelma@users.noreply.github.com>
2025-09-17 10:33:33 -0400

    wed sep 17 25

---
+++ Lesson_03_Flask_App_Structure/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Home Page!"
+
+app.run()
+
+@app.route('/')
+def about():
+    # return "About Page!"
+    return """<h1>Welcome<h1>
+                    <p>This is my first website<p>
+            """
+
+
+
+@app.route('/contact')
+def contact():
+    return "Contact Page"
+app.run(delog=True)
